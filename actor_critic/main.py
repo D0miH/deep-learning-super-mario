3680104f0e346de1c72b529713894815dd849958
Dominik <36657497+D0miH@users.noreply.github.com>
2019-08-06 15:54:29 +0200

    removed batch normalization

--- actor_critic/main.py
+++ actor_critic/main.py
@@ -116,15 +116,12 @@ class ActorCriticNet(nn.Module):
 
         # create a convolution net
         self.conv1 = nn.Conv2d(in_channels=FRAME_DIM[2], out_channels=32, kernel_size=8, stride=2)
-        self.conv1_bn = nn.BatchNorm2d(32)
 
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
-        self.conv2_bn = nn.BatchNorm2d(64)
 
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2)
-        self.conv3_bn = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1)
 
-        self.fc1 = nn.Linear(in_features=21504, out_features=512)
+        self.fc1 = nn.Linear(in_features=89600, out_features=512)
 
         # create one output for the actor
         self.head_actor = nn.Linear(in_features=512, out_features=num_actions)
@@ -133,14 +130,12 @@ class ActorCriticNet(nn.Module):
         self.head_critic = nn.Linear(in_features=512, out_features=1)
 
     def forward(self, x):
-        out = self.conv1(x)
-        out = F.elu(self.conv1_bn(out))
+        out = F.elu(self.conv1(x))
 
-        out = self.conv2(out)
-        out = F.elu(self.conv2_bn(out))
+        out = F.elu(self.conv2(out))
 
-        out = self.conv3(out)
-        out = F.elu(self.conv3_bn(out))
+        out = F.elu(self.conv3(out))
+
         out = out.view(out.size()[0], -1)
 
         out = F.elu(self.fc1(out))
@@ -175,6 +170,11 @@ for episode in range(1, NUM_EPOCHS):
         del state
         state, reward, done, info = env.step(action)
 
+        if done or info["life"] < 2 or step >= MAX_STEPS_PER_EPOCH:
+            reward_history.append(last_reward)
+            reward_mean_history.append(np.mean(reward_history))
+            break
+
         state = lazy_frame_to_tensor(state)
 
         if RENDER_GAME:
@@ -182,11 +182,6 @@ for episode in range(1, NUM_EPOCHS):
 
         step_reward_history.append(reward)
         last_reward += reward
-
-        if done or info["life"] < 2 or step >= MAX_STEPS_PER_EPOCH:
-            reward_history.append(last_reward)
-            reward_mean_history.append(np.mean(reward_history))
-            break
 
     if episode % LOG_INTERVAL == 0:
         print("Episode {}\tLast Reward: {:.2f}\tAverage reward: {:.2f}".format(episode, last_reward,
