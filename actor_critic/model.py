a34bfb20e1b4aabbc6d5d8e3fa39f959e2c8f30b
Dominik <36657497+D0miH@users.noreply.github.com>
2019-08-09 10:15:22 +0200

    size of the output of the cnn is now automatically calculated

--- actor_critic/model.py
+++ actor_critic/model.py
@@ -7,20 +7,31 @@ class ActorNet(nn.Module):
     Neural net to select the next action to take.
     The output are logits. You should use softmax to get the probability distribution.
     """
+
     def __init__(self, num_actions, frame_dim):
         super(ActorNet, self).__init__()
 
         # create a convolution net
-        self.conv1 = nn.Conv2d(in_channels=frame_dim[2], out_channels=32, kernel_size=8, stride=2)
+        self.conv1 = nn.Conv2d(in_channels=frame_dim[2], out_channels=32, kernel_size=2, stride=1)
         self.conv1_bn = nn.BatchNorm2d(32)
 
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
-        self.conv2_bn = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=2, stride=1)
+        self.conv2_bn = nn.BatchNorm2d(32)
 
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1)
-        self.conv3_bn = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=2, stride=1)
+        self.conv3_bn = nn.BatchNorm2d(32)
 
-        self.fc1 = nn.Linear(in_features=89600, out_features=512)
+        def conv2d_size_out(size, kernel_size=5, stride=2):
+            return (size - (kernel_size - 1) - 1) // stride + 1
+
+        # calculate the output size of the last conv layer
+        conv_width = conv2d_size_out(
+            conv2d_size_out(conv2d_size_out(frame_dim[1], kernel_size=2, stride=1), kernel_size=2, stride=1),
+            kernel_size=2, stride=1)
+        conv_height = conv2d_size_out(
+            conv2d_size_out(conv2d_size_out(frame_dim[0], kernel_size=2, stride=1), kernel_size=2, stride=1),
+            kernel_size=2, stride=1)
+        self.fc1 = nn.Linear(in_features=conv_width * conv_height * 32, out_features=512)
 
         # create one output for the actor
         self.head_actor = nn.Linear(in_features=512, out_features=num_actions)
@@ -54,16 +65,27 @@ class CriticNet(nn.Module):
         super(CriticNet, self).__init__()
 
         # create a convolution net
-        self.conv1 = nn.Conv2d(in_channels=frame_dim[2], out_channels=32, kernel_size=8, stride=2)
+        self.conv1 = nn.Conv2d(in_channels=frame_dim[2], out_channels=32, kernel_size=2, stride=1)
         self.conv1_bn = nn.BatchNorm2d(32)
 
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
-        self.conv2_bn = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=2, stride=1)
+        self.conv2_bn = nn.BatchNorm2d(32)
 
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1)
-        self.conv3_bn = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=2, stride=1)
+        self.conv3_bn = nn.BatchNorm2d(32)
 
-        self.fc1 = nn.Linear(in_features=89600, out_features=512)
+        def conv2d_size_out(size, kernel_size=5, stride=2):
+            return (size - (kernel_size - 1) - 1) // stride + 1
+
+        # calculate the output size of the last conv layer
+        conv_width = conv2d_size_out(
+            conv2d_size_out(conv2d_size_out(frame_dim[1], kernel_size=2, stride=1), kernel_size=2, stride=1),
+            kernel_size=2, stride=1)
+        conv_height = conv2d_size_out(
+            conv2d_size_out(conv2d_size_out(frame_dim[0], kernel_size=2, stride=1), kernel_size=2, stride=1),
+            kernel_size=2, stride=1)
+
+        self.fc1 = nn.Linear(in_features=conv_width * conv_height * 32, out_features=512)
 
         self.head_critic = nn.Linear(in_features=512, out_features=1)
 
@@ -85,4 +107,3 @@ class CriticNet(nn.Module):
         critic_out = self.head_critic(out)
 
         return critic_out
-
---
+++ actor_critic/space_invaders_test.py
@@ -0,0 +1,112 @@
+from itertools import count
+import gym
+import atari_py
+
+from wrappers import wrapper
+from actor_critic.agent import Agent
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+# env settings
+LEVEL_NAME = "SpaceInvaders-v0"
+FRAME_DIM = (84, 84, 4)  # original image size is 240x256
+RENDER_GAME = True
+
+# training hyperparameters
+ACTOR_LEARNING_RATE = 0.03  # gradient seems to be exploding :/ in particular the entropy loss is exploding maybe we could clip the reward
+CRITIC_LEARNING_RATE = 0.03
+NUM_EPOCHS = 1000
+GAMMA = 0.99  # the discount factor
+BETA = 1  # the scaling of the entropy
+ZETA = 1  # the scaling of the value loss
+TRAIN_INNTERVAL = 10_000  # number of steps after which the agent is trained
+
+LOG_INTERVAL = 1
+PLOT_INTERVAL = 1
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def create_environment():
+    """Creates the environment, applies some wrappers and returns it."""
+    tmp_env = gym.make(LEVEL_NAME)
+    tmp_env = wrapper(tmp_env, FRAME_DIM)
+
+    return tmp_env
+
+
+def lazy_frame_to_tensor(lazy_frame):
+    # pytorch expects the frames as height x width x depth
+    return torch.from_numpy(
+        np.expand_dims(np.asarray(lazy_frame).astype(np.float64).transpose((2, 1, 0)), axis=0)).float().to(DEVICE)
+
+
+def plot_rewards(reward_list, given_reward_mean_history):
+    plt.plot(reward_list, "b-", given_reward_mean_history, "r-")
+    plt.ylabel("Rewards")
+    plt.xlabel("Episodes")
+    plt.legend(["Reward per episode", "Average reward"])
+    plt.show()
+
+
+env = create_environment()
+env.seed(1)
+torch.manual_seed(1)
+
+agent = Agent(env.action_space.n, FRAME_DIM, GAMMA, BETA, ZETA, ACTOR_LEARNING_RATE, CRITIC_LEARNING_RATE, DEVICE)
+
+reward_history = []
+reward_mean_history = []
+
+trajectory_history = []
+total_steps = 0
+for episode in range(1, NUM_EPOCHS):
+    state = lazy_frame_to_tensor(env.reset())
+
+    episode_reward = 0
+    trajectory = []  # in here all the transitions are stored as [[state, action, reward, next_state, done],...]
+    for step in count(1):
+        # get the next action to perform
+        action = agent.get_action(state)
+
+        # perform the action and get the feedback from the environment
+        next_state, reward, done, info = env.step(action)
+        next_state = lazy_frame_to_tensor(next_state)
+
+        trajectory.append([state, action, reward, next_state, done])
+
+        # update the reward of the episode
+        episode_reward += reward
+
+        # update the state and delete the previous one
+        state = next_state
+
+        total_steps += 1
+
+        if RENDER_GAME:
+            env.render()
+
+        if done:
+            reward_history.append(episode_reward)
+            reward_mean_history.append(np.mean(reward_history))
+            break
+
+        if total_steps % TRAIN_INNTERVAL == 0:
+            agent.update(trajectory_history)
+            del trajectory_history[:]
+
+    trajectory_history.append(trajectory)
+
+    # log some info
+    if episode % LOG_INTERVAL == 0:
+        print("Episode {}\tReward: {:.2f}\tAverage reward: {:.2f}\tActor Loss: {:.2f}\tCritic Loss: {:.2f}".format(
+            episode, episode_reward,
+            reward_mean_history[-1]))
+
+    if episode % PLOT_INTERVAL == 0:
+        plot_rewards(reward_history, reward_mean_history)
+        agent.plot_loss()
+
+    torch.cuda.empty_cache()
